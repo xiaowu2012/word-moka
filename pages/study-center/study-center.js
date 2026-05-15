ac5f1659feed0116949c078a8743ad270d001476
Linghua <wang@LinghuadeMac-mini.local>
2026-05-15 15:16:56 +0800

    模块选择改为下拉框(picker)

--- pages/study-center/study-center.js
+++ pages/study-center/study-center.js
@@ -14,7 +14,8 @@ Page({
     showGoalPicker: false,
     modules: [],
     selectedModuleId: '',
-    selectedModuleName: '全部模块',
+    selectedModuleName: '全部',
+    moduleOptions: [],
     words: {}
   },
 
@@ -31,12 +32,17 @@ Page({
       m.wordCount = cnt
     })
 
+    // picker 选项
+    const moduleOptions = ['全部', ...modules.map(m => `${m.icon} ${m.name}`)]
+
     this.setData({
       textbook,
       totalWords: total,
       dailyGoal: wx.getStorageSync('dailyGoal') || DEFAULT_GOAL,
       modules,
+      moduleOptions,
       selectedModuleId: '',
+      selectedModuleName: '全部',
       words
     })
 
@@ -92,13 +98,19 @@ Page({
     }).catch(() => {})
   },
 
-  onModuleSelect(e) {
-    const id = e.currentTarget.dataset.id
+  onModuleChange(e) {
+    const idx = e.detail.value
     const modules = this.data.modules
-    const mod = modules.find(m => m.id === id)
+    let id = ''
+    let name = '全部'
+    if (idx > 0) {
+      const mod = modules[idx - 1]
+      id = mod.id
+      name = `${mod.icon} ${mod.name}`
+    }
     this.setData({
-      selectedModuleId: id || '',
-      selectedModuleName: mod ? `${mod.icon} ${mod.name}` : '全部模块'
+      selectedModuleId: id,
+      selectedModuleName: name
     }, () => {
       this.loadProgress()
     })
